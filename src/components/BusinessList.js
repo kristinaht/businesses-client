0b01b4ecc6a6ea56ca644410cb15f2c1772446bc
Kristina Hengster Tintor <hengster.tintor@gmail.com>
2020-05-13 12:32:56 -0700

    correct props.businessList.map in BusinessList component

--- src/components/BusinessList.js
+++ src/components/BusinessList.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Business from './Business';
 
 function BusinessList(props) {
   return(
     <React.Fragment>
       <ul>
-        {props.businesses.map((business, index) =>
+        {props.businessList.map((business, index) =>
           <li key={index}>
             <h2 key={index}>{business.name}</h2>
             <h3>{business.description}</h3>
