7014c7974e1e13f239f285900b7ffd368717c8a8
Kristina Hengster Tintor <hengster.tintor@gmail.com>
2020-05-13 09:30:20 -0700

    add makeApiCall to actions/index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -13,3 +13,17 @@ export const getBusinessesFail = (error) => ({
   type: c.GET_BUSINESSES_FAIL,
   error
 })
+
+export const makeApiCall = () => {
+  return dispatch => {
+    dispatch(requestBusinesses);
+    return fetch("http://localhost:5000/api/businesses")
+      .then(response => response.json())
+      .then((jsonifiedResponse) => {
+        dispatch(getBusinessesSuccess(jsonifiedResponse));
+      })
+    .catch((error) => {
+      dispatch(getBusinessesFail(error));
+    });
+  }
+}
